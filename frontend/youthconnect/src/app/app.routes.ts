ae537030a7e2483274d891481e92489df0721f83
consultado-kirby <kirbyconsultado@gmail.com>
2026-01-12 20:46:04 +0800

    feat: connect the login-page component to the backend

--- frontend/youthconnect/src/app/app.routes.ts
+++ frontend/youthconnect/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
 import { LoginPage } from './pages/login-page/login-page';
+import { Dashboard as YouthDashboard } from './pages/youth-member/dashboard/dashboard';
+import { Dashboard as SkDashboard } from './pages/sk-official/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', component: LandingPage },
   { path: 'login', component: LoginPage },
+  { path: 'youth-member/dashboard', component: YouthDashboard },
+  { path: 'sk-official/dashboard', component: SkDashboard },
 ];
