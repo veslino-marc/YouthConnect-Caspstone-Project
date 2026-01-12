0b1cb4bcf1263a3cc0e4dfa725356768363b2d37
consultado-kirby <kirbyconsultado@gmail.com>
2026-01-12 18:40:00 +0800

    feat: add login-page style and behavior

--- frontend/youthconnect/src/app/app.ts
+++ frontend/youthconnect/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
