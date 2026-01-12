82b50adbfe9dfd1d92e09ab36ec1071da30280ac
consultado-kirby <kirbyconsultado@gmail.com>
2026-01-12 18:58:53 +0800

    feat: add initial landing page

--- frontend/youthconnect/src/app/app.ts
+++ frontend/youthconnect/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { LandingPage } from './pages/landing-page/landing-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPage],
+  imports: [RouterOutlet, LoginPage, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
